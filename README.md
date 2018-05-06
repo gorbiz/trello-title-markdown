708b77624540a6721fd83e3381a97071acc8b1cc
Karl Svartholm <karl@int3software.com>
2018-05-06 12:17:07 +0200

    adding icons + minor tweaks

---
+++ README.md
@@ -0,0 +1,4 @@
+# Build
+```sh
+zip trello-markdown-title.zip *.js *.json *.png
+```
--- content.js
+++ content.js
@@ -8,15 +8,15 @@
 // ==/UserScript==
 
 function markdownAll() {
-	var cards = document.getElementsByClassName('list-card-title');
-	for (var i = 0; i < cards.length; i++) {
-		cards[i].innerHTML = cards[i].innerHTML
-		    .replace(/\*\*(.+?)\*\*/g, '<strong>$1</strong>')
-		    .replace(/\*(.+?)\*/g, '<em>$1</em>')
+  var cards = document.getElementsByClassName('list-card-title');
+  for (var i = 0; i < cards.length; i++) {
+  cards[i].innerHTML = cards[i].innerHTML
+      .replace(/\*\*(.+?)\*\*/g, '<strong>$1</strong>')
+      .replace(/\*(.+?)\*/g, '<em>$1</em>')
         .replace(/\`(.+?)\`/g, '<code>$1</code>')
-		    .replace(/~~(.+?)~~/g, '<strike>$1</strike>');
-	}
-	setTimeout(markdownAll, 500);
+      .replace(/~~(.+?)~~/g, '<strike>$1</strike>');
+  }
+  setTimeout(markdownAll, 500);
 }
 
 markdownAll();
