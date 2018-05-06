fda943b23232b650f5d81fc0db81f308866ae3fa
Karl Svartholm <karl@int3software.com>
2018-05-06 13:40:04 +0200

    :shirt: fix lint -> standard js

--- content.js
+++ content.js
@@ -1,12 +1,12 @@
-function markdownAll() {
-  var cards = document.getElementsByClassName('list-card-title');
+function markdownAll () {
+  var cards = document.getElementsByClassName('list-card-title')
   for (var i = 0; i < cards.length; i++) {
-  cards[i].innerHTML = cards[i].innerHTML
+    cards[i].innerHTML = cards[i].innerHTML
       .replace(/\*\*(.+?)\*\*/g, '<strong>$1</strong>')
       .replace(/\*(.+?)\*/g, '<em>$1</em>')
-        .replace(/\`(.+?)\`/g, '<code>$1</code>')
-      .replace(/~~(.+?)~~/g, '<strike>$1</strike>');
+      .replace(/`(.+?)`/g, '<code>$1</code>')
+      .replace(/~~(.+?)~~/g, '<strike>$1</strike>')
   }
-  setTimeout(markdownAll, 500);
+  setTimeout(markdownAll, 500)
 }
-markdownAll();
+markdownAll()
